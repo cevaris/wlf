6c076c68e70e6b94083544086c002da75100bf9b
Adam Cardenas <cevaris@gmail.com>
2015-04-23 08:01:53 -0600

    got working name rendering for auth'ed users

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def current_account
+    if user_signed_in? and current_user
+      return current_user.account
+    else
+      return false
+    end
+  end
 end
--- app/models/account.rb
+++ app/models/account.rb
@@ -2,4 +2,8 @@ class Account < ActiveRecord::Base
   belongs_to :user
   validates :first_name, presence: true, allow_blank: false
   validates :last_name, presence: true, allow_blank: true
+
+  def full_name
+    "#{self.first_name.titleize} #{self.last_name.titleize}"
+  end
 end
