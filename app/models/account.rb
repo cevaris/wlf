f37bc707336d59ff7efe8a7f6e622f5664e110ca
Adam Cardenas <cevaris@gmail.com>
2015-05-03 20:05:53 -0600

    Added destroy callbacks to user->account->x dependents

--- app/models/account.rb
+++ app/models/account.rb
@@ -8,8 +8,11 @@ class Account < ActiveRecord::Base
   belongs_to :account_type
   validates :account_type, presence: true, allow_blank: false
 
-  belongs_to :address
+  belongs_to :address, dependent: :destroy
   accepts_nested_attributes_for :address
+
+  has_many :events, dependent: :destroy
+  has_many :event_submissions, dependent: :destroy
 
   validates :first_name, presence: true, allow_blank: false
   validates :last_name, presence: true, allow_blank: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
-  has_one :account
+  has_one :account, dependent: :destroy
   accepts_nested_attributes_for :account
   validates :account, presence: true
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
